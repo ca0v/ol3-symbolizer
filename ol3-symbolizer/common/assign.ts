9483f8eb33f72b45876892f4310bc06a7ce2b541
calix <Corey.Alix@infor.com>
2018-08-28 10:53:09 -0400

    a star w/ infinate points degrades to a circle

--- ol3-symbolizer/common/assign.ts
+++ ol3-symbolizer/common/assign.ts
@@ -4,19 +4,24 @@
  * @param prop The property name
  * @param value The property value
  */
-export function assign(obj: any, prop: string, value: Object) {
-    //let getter = prop[0].toUpperCase() + prop.substring(1);
+export function assign(obj: any, prop: string, value: any) {
     if (value === null) return;
     if (value === undefined) return;
+
     if (typeof value === "object") {
         if (Object.keys(value).length === 0) return;
     }
+
     if (prop === "image") {
         if (value.hasOwnProperty("radius")) {
             prop = "circle";
         }
+
         if (value.hasOwnProperty("points")) {
-            prop = "star";
+            let points = value["points"];
+            if (points < Infinity) {
+                prop = "star";
+            }
         }
     }
     obj[prop] = value;
